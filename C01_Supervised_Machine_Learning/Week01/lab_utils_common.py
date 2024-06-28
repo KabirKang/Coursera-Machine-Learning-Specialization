1af371c98148326ea970e5fd7b3d49b15c9e4213
Kabir Kang <kangkabir@gmail.com>
2024-06-28 15:33:21 -0700

    Gradient Descent

---
+++ C01_Supervised_Machine_Learning/Week01/lab_utils_common.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+dlblue = '#0096ff'; dlorange = '#FF9300'; dldarkred='#C00000'; dlmagenta='#FF40FF'; dlpurple='#7030A0';
+dlcolors = [dlblue, dlorange, dldarkred, dlmagenta, dlpurple]
